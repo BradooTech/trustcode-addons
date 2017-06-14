a57f4659fb94ce0693e26450f115c6a91329ecd7
Unknown <gabriel.balog@bradootech.com>
2017-06-14 11:18:11 -0300

    Modificação no cielo

--- br_payment_cielo/models/cielo.py
+++ br_payment_cielo/models/cielo.py
@@ -204,7 +204,7 @@ class TransactionCielo(models.Model):
         state = 'pending' if state_cielo == '1' else 'error'
         state = 'done' if state_cielo in ('2', '7') else state
 
-        x = self.env['sale.order'].search([('name','=','reference')]).write({'payment_status':state_cielo,'cielo_return':True})
+        x = self.env['sale.order'].search([('name','=',reference)]).write({'payment_status':state_cielo,'cielo_return':True})
         print x
         
         if state == 'done':
