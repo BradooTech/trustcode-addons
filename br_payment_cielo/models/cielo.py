792ff2b03d2003db830556af4ff844b138dbe830
Unknown <gabriel.balog@bradootech.com>
2017-06-20 18:48:24 -0300

    Validação de status para criação do invoice e atualização dos preços

--- br_payment_cielo/models/cielo.py
+++ br_payment_cielo/models/cielo.py
@@ -10,6 +10,7 @@ from odoo.http import request
 from datetime import datetime
 from datetime import timedelta
 from dateutil.relativedelta import relativedelta
+from odoo.tools import DEFAULT_SERVER_DATE_FORMAT
 
 
 _logger = logging.getLogger(__name__)
@@ -207,20 +208,30 @@ class TransactionCielo(models.Model):
         self.env['sale.order'].search([('name','=',reference)]).write({'payment_status': state_cielo,'cielo_return':True})
         so = self.env['sale.order'].search([('name','=',reference)])
         
+        if amount != so.amount_total:
+            # so.write({'amount': amount})
+            amount_dif = so.amount_total - amount
+            lines = len(so.order_line)
+            dif = amount_dif / lines
+            sub = self.sale_order_id.subscription_id
+            for sub_line in sub.recurring_invoice_line_ids:
+                price_final = sub_line.price_subtotal - dif
+                sub_line.write({'price_subtotal': price_final, 'price_unit': price_final})
+            for line in so.order_line:
+                price_final = line.price_subtotal - dif
+                line.write({'price_subtotal': price_final, 'price_unit': price_final, 'valor_bruto': price_final})
+
+            sub.write({'recurring_total':amount})
+
+
         if state == 'done':
             if not so.subscription_id:
                 so.action_confirm()
             if state_cielo == '2':
                 self.create_invoice_nfse()
+                sub = self.sale_order_id.subscription_id
+                sub.write({'recurring_next_date': (datetime.strptime(sub.recurring_next_date, DEFAULT_SERVER_DATE_FORMAT) + relativedelta(months=1))})
         
-        if amount != so.amount_total:
-            so.write({'amount': amount})
-            amount_dif = so.amount_total - amount
-            lines = len(so.order_line)
-            dif = amount_dif / lines
-            for line in so.order_line:
-                price_final = line.price_subtotal - dif
-                line.write({'price_subtotal': price_final, 'price_unit': price_final, 'valor_bruto': price_final})
             
             
 
@@ -287,7 +298,7 @@ class TransactionCielo(models.Model):
                         'name': line.name,
                         'origin': self.sale_order_id.name,
                         'account_id': line.product_id.property_account_income_id.id,
-                        'price_unit': line.product_id.lst_price,
+                        'price_unit': line.price_subtotal,
                         'quantity': line.product_uom_qty,
                         #'discount': 0.0,
                         'uom_id': line.product_id.uom_id.id,
@@ -304,48 +315,48 @@ class TransactionCielo(models.Model):
                         #'invoice_line_tax_ids': [(6, 0, line.product_id.tax_ids)],
                         }))
             
-            invoice = self.env['account.invoice'].create({
-                'partner_id': self.sale_order_id.partner_id.id,
-                'fiscal_document_id': 35, #preencher
-                'document_serie_id': 3,     #preencher
-                'account_id': self.sale_order_id.partner_id.property_account_receivable_id.id,
-                'invoice_line_ids':invoice_line_ids_construct,
-                'date_invoice': datetime.now().strftime("%Y-%m-%d"),
-                'origin': self.sale_order_id.subscription_id.code
-                })
-                
-            ''' COMENTAR CASO NECESSITE REMOVER A EMISSAO DE NF 
-            Comentando para remoção dos dados de envio e geração de nota fiscal'''
-            # invoice.action_invoice_open()
+        invoice = self.env['account.invoice'].create({
+            'partner_id': self.sale_order_id.partner_id.id,
+            'fiscal_document_id': 35, #preencher
+            'document_serie_id': 3,     #preencher
+            'account_id': self.sale_order_id.partner_id.property_account_receivable_id.id,
+            'invoice_line_ids':invoice_line_ids_construct,
+            'date_invoice': datetime.now().strftime("%Y-%m-%d"),
+            'origin': self.sale_order_id.subscription_id.code
+            })
             
+        ''' COMENTAR CASO NECESSITE REMOVER A EMISSAO DE NF 
+        Comentando para remoção dos dados de envio e geração de nota fiscal'''
+        # invoice.action_invoice_open()
+        
 
-            # edoc = self.env['invoice.eletronic'].search([('invoice_id','=',invoice.id)])
-            
-            # ReportXml = self.env['ir.actions.report.xml']
-            # Report = self.env['report']
-            
-            # for doc in edoc:
-            #     if doc.state == 'draft':
-            #         doc.action_send_eletronic_invoice()
-                    
-            #         report = ReportXml.search([('model', '=', 'invoice.eletronic'),('name','=','Impressao de NFS-e Paulistana')], limit=1)
-            #         bin_pdf = Report.get_pdf([doc.id], 'ir_csll_bradoo.main_template_br_nfse_danfe')
-            #         pdf_final = bin_pdf.encode('base64')
-            #         attach = self.env['ir.attachment'].create({
-            #             'name':'NFse ' + str(doc.partner_id.name),
-            #             'res_model':'invoice.eletronic',
-            #             'type':'binary',
-            #             'datas_fname': 'Nfse.pdf',
-            #             'res_id': doc.id,
-            #             'datas': pdf_final,
-            #             'res_name':'NFse'
-            #         })
-                    
-            #         attachment_ids = self.env['ir.attachment'].search([('res_id','=', doc.id),('res_model','=','invoice.eletronic'),('mimetype','=','application/pdf')])
-            #         template_id = self.env.ref('br_payment_cielo.mail_template_data_lexis_nfse')
-            #         mail_template = self.env['mail.template'].browse(template_id.id)
-            #         mail_id = mail_template.send_mail(self.id)
-            #         mail = self.env['mail.mail'].browse(mail_id)
-            #         mail.update({
-            #             'attachment_ids':[(4, attachment.id) for attachment in attachment_ids]
-            #         })
+        # edoc = self.env['invoice.eletronic'].search([('invoice_id','=',invoice.id)])
+        
+        # ReportXml = self.env['ir.actions.report.xml']
+        # Report = self.env['report']
+        
+        # for doc in edoc:
+        #     if doc.state == 'draft':
+        #         doc.action_send_eletronic_invoice()
+                
+        #         report = ReportXml.search([('model', '=', 'invoice.eletronic'),('name','=','Impressao de NFS-e Paulistana')], limit=1)
+        #         bin_pdf = Report.get_pdf([doc.id], 'ir_csll_bradoo.main_template_br_nfse_danfe')
+        #         pdf_final = bin_pdf.encode('base64')
+        #         attach = self.env['ir.attachment'].create({
+        #             'name':'NFse ' + str(doc.partner_id.name),
+        #             'res_model':'invoice.eletronic',
+        #             'type':'binary',
+        #             'datas_fname': 'Nfse.pdf',
+        #             'res_id': doc.id,
+        #             'datas': pdf_final,
+        #             'res_name':'NFse'
+        #         })
+                
+        #         attachment_ids = self.env['ir.attachment'].search([('res_id','=', doc.id),('res_model','=','invoice.eletronic'),('mimetype','=','application/pdf')])
+        #         template_id = self.env.ref('br_payment_cielo.mail_template_data_lexis_nfse')
+        #         mail_template = self.env['mail.template'].browse(template_id.id)
+        #         mail_id = mail_template.send_mail(self.id)
+        #         mail = self.env['mail.mail'].browse(mail_id)
+        #         mail.update({
+        #             'attachment_ids':[(4, attachment.id) for attachment in attachment_ids]
+        #         })
